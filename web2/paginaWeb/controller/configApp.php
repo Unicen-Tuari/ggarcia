c5603f0e63c325657a0b2028fd9b124e17785a56
Gabriel <gabriel.c.garcia@gmail.com>
2016-08-24 07:54:11 -0300

    close #14 Empezamos proyecto web 2 con MVC

---
+++ web2/paginaWeb/controller/configApp.php
@@ -0,0 +1,9 @@
+<?php
+  class ConfigApp {
+    public static $ACTION = 'action';
+    public static $ACTION_HOME = 'home';
+    public static $ACTION_DANCE = 'dance';
+    public static $ACTION_CONTACT_US = 'contact_us';
+    public static $ACTION_NEW_TASK = 'nueva_tarea'; // esta la podemos sacar
+  }
+?>
---
+++ web2/paginaWeb/index.php
@@ -0,0 +1,10 @@
+<?php
+  require_once "controller/configApp.php";
+  require_once "controller/home_controller.php";
+
+  if (!array_key_exists(ConfigApp::$ACTION,$_REQUEST) || $_REQUEST[ConfigApp::$ACTION] == ConfigApp::$ACTION_HOME) {
+    // Home del sitio
+    $homeController = new HomeController();
+    $homeController->home();
+  } // else{...}
+?>
