8f7220e5cddc361f379a30b33348bfbb6b3c73d4
Gabriel <gabriel.c.garcia@gmail.com>
2016-08-31 11:04:28 -0300

    Comenzamos practico

--- web2/paginaWeb/config/configApp.php
+++ web2/paginaWeb/config/configApp.php
@@ -4,6 +4,6 @@
     public static $ACTION_HOME = 'home';
     public static $ACTION_DANCE = 'dance';
     public static $ACTION_CONTACT_US = 'contact_us';
-    public static $ACTION_NEW_TASK = 'nueva_tarea'; // esta la podemos sacar
+    // public static $ACTION_NEW_TASK = 'nueva_tarea'; // esta la podemos sacar
   }
 ?>
