4daf23435b5a0165e3fb1c26018eda644e41aa5d
Gabriel Garcia <gabriel_garcia@EDSA-AR.com>
2016-09-05 09:16:15 -0300

    TP2 Ejercicio 2a

--- web2/Practico02-SMARTY/personaA.php
+++ web2/Practico02-SMARTY/personaA.php
@@ -2,7 +2,7 @@
 	require("../libs/Smarty.class.php");
   $info = [];
 
-	$smarty = new Smarty;
+	//$smarty = new Smarty;
 
   if (isset($_POST["inversion"]) && isset($_POST["interes"])) {
     $info = [
@@ -12,7 +12,9 @@
     ];
   }
 
-  $smarty->assign("titulo","Ejercicio 2");
+  /*
+	$smarty->assign("titulo","Ejercicio 2");
   $smarty->assign('info',$info);
 	$smarty->display('../templates/persona.tpl');
+	*/
 ?>
