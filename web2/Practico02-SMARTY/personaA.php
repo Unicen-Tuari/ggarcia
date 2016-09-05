57487f43f9702b1c44f38b96ea83854a958a2c51
Gabriel Garcia <gabriel_garcia@EDSA-AR.com>
2016-09-05 14:08:35 -0300

    TP2 ej2 A print_r()

--- web2/Practico02-SMARTY/personaA.php
+++ web2/Practico02-SMARTY/personaA.php
@@ -1,15 +1,19 @@
 <?php
 	require("../libs/Smarty.class.php");
-  $info = [];
 
 	//$smarty = new Smarty;
 
-  if (isset($_POST["inversion"]) && isset($_POST["interes"])) {
-    $info = [
-      "name" => $_POST["Nombre"],
-      "lastName" => $_POST["Apellido"],
-      "birthDate" => $_POST["fecha"]
-    ];
+  if (isset($_POST["Nombre"]) && isset($_POST["Apellido"]) && isset($_POST["fecha"])) {
+    print_r($_POST["Nombre"] + " " + $_POST["Apellido"]);
+		print_r("Fecha de nacimiento: " + $_POST["fecha"]);
+		print_r("Nació un " + date("l",$_POST["fecha"]));
+		print_r(date_diff(getdate(),$_POST["fecha"]));
+		/*print_r(); // semanas
+		print_r(); // meses
+		print_r(); // años
+		*/
+  } else {
+  	print_r("Error - Debe cargar los datos correctamente.");
   }
 
   /*
@@ -18,3 +22,8 @@
 	$smarty->display('../templates/persona.tpl');
 	*/
 ?>
+
+<p>Tiene días 6.572 Días</p>
+<p>938,86 Semanas</p>
+<p>215,92 Meses</p>
+<p>18 Años</p>
