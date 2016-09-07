544f1898330306e05cb177e8797e2a08646acdec
Gabriel Garcia <gabriel_garcia@EDSA-AR.com>
2016-09-07 17:35:59 -0300

    TP2 ej3 probando con js

--- web2/Practico02-SMARTY/ej3.php
+++ web2/Practico02-SMARTY/ej3.php
@@ -4,4 +4,12 @@
   $smarty = new Smarty;
 	$smarty->display('../templates/ej3.tpl');
 
+	function IdentificarDominio() {
+		$dominio = explode("@",$_POST["Email"]);
+		if (($dominio[1] == "gmail.com") || ($dominio[1] == "hotmail.com")) {
+			return true;
+		}
+		return false;
+	}
+
 ?>
