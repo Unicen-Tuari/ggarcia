c6ca2c81f9b1e0496c68b439b008a0f6b9debefa
Gabriel <gabriel.c.garcia@gmail.com>
2016-09-15 08:02:28 -0200

    TPweb completamos index con las claves de los request

--- web2/paginaWeb/index.php
+++ web2/paginaWeb/index.php
@@ -1,10 +1,30 @@
 <?php
   require_once("config/configApp.php");
   require_once("controller/home_controller.php");
+  require_once("controller/dance_controller.php");
+  require_once("controller/contact_controller.php");
 
-  if (!array_key_exists(ConfigApp::$ACTION,$_REQUEST) || $_REQUEST[ConfigApp::$ACTION] == ConfigApp::$ACTION_HOME) {
-    // Home del sitio
-    $homeController = new HomeController();
-    $homeController->home();
-  } // else{...} para todos los menúes?!?!?! no conviene con un switch?
+  if (!array_key_exists(ConfigApp::$ACTION,$_REQUEST)) {
+    switch ($_REQUEST[ConfigApp::$ACTION]) {
+      // Home del sitio
+      case (ConfigApp::$ACTION_HOME):
+        $homeController = new HomeController();
+        $homeController->home();
+        break;
+      // Clases del sitio
+      case (ConfigApp::$ACTION_DANCE):
+        $danceController = new DanceController();
+        $danceController->clases();
+        break;
+      case (ConfigApp::$ACTION_CONTACT_US):
+        $contactController = new ContactController();
+        $contactController->contactUs();
+        break;
+      default:
+        echo "No se igualó clave de request";
+    }
+  } else {
+    echo "No se encontró clave"
+  }
+
 ?>
