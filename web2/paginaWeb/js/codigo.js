f90d545ce5f39393932f7957d7bddbffb294cdbc
Gabriel <gabriel.c.garcia@gmail.com>
2016-10-05 16:47:40 -0200

    resolved #18 Actualizamos función de AJAX

--- web2/paginaWeb/js/codigo.js
+++ web2/paginaWeb/js/codigo.js
@@ -1,7 +1,15 @@
-// cada vez que elijo botón de navigation bar se carga su correspondiente render
-$("#home").on("click",function() {webLoader("index.php?action=home")});
-$("#clase").on("click",function() {webLoader("index.php?action=dance")});
-$("#contact").on("click",function() {webLoader("index.php?action=contact_us")});
+$(document).ready(function() {
+  // cada vez que elijo botón de navigation bar se carga su correspondiente render
+  $("#home").on("click",function() {
+    event.preventDefault();
+    webLoader("index.php?action=home")});
+  $("#clase").on("click",function() {
+    event.preventDefault();
+    webLoader("index.php?action=dance")});
+  $("#contact").on("click",function() {
+    event.preventDefault();
+    webLoader("index.php?action=contact_us")});
+});
 
 // fn que obtiene html a cargar en un div agregando funcionalidad
 function webLoader(link) {
@@ -10,21 +18,10 @@ function webLoader(link) {
       url: link,
       dataType: "html",
       success: function(data) {
-        $("#partialRender").html(data);
-        switch(solapa) { // debo agregar funcionalidad a las páginas que lo necesitan
-          case clase: {
-            setFuncionalidadClases();
-            break;
-          }
-          case inscripcion: {
-            getInscripciones(); //muestro info en tabla
-            $("#saveInscripcion").on("click",function() {guardarInscripcion()});
-            break;
-          }
-        }
+        $("#content").html(data);
       },
-      error:function(jqxml, status, errorThrown){
-        $("#partialRender").text("No se pudo cargar la página");
+      error: function(jqxml, status, errorThrown) {
+        $("#content").text("No se pudo cargar la página");
         console.log(errorThrown);
       }
     });
